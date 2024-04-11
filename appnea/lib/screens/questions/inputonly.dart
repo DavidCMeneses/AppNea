7998e5550c3d1dce1c019951fd4940d1817a0178
almartins1 <almartins@unal.edu.co>
2024-04-10 20:01:04 -0500

    Mejoren
    Mejoren

--- appnea/lib/screens/questions/inputonly.dart
+++ appnea/lib/screens/questions/inputonly.dart
@@ -3,6 +3,18 @@ import 'package:appnea/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+bool isNumericFloat(String string) {
+  final numericRegex = RegExp(r'^-?(([0-9]*)|(([0-9]*)(\.|\,)([0-9]*)))$');
+
+  return numericRegex.hasMatch(string.trim());
+}
+
+bool isNumericInt(String string) {
+  final numericRegex = RegExp(r'^[0-9]+$');
+
+  return numericRegex.hasMatch(string.trim());
+}
+
 class InputPage extends StatelessWidget {
   final String questNum;
   final String questText;
@@ -26,53 +38,74 @@ class InputPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 238, 241, 239),
-          scrolledUnderElevation : 0,
-        flexibleSpace: CustomAppBar(infoPage: infopage,),
+          backgroundColor: const Color.fromARGB(255, 238, 241, 239),
+          scrolledUnderElevation: 0,
+          flexibleSpace: CustomAppBar(
+            infoPage: infopage,
+          ),
           bottom: PreferredSize(
             preferredSize: const Size.fromHeight(30),
             child: Text(questTitle + 'Pregunta #' + questNum,
-                style: const TextStyle(fontSize: qtextsize, fontWeight: FontWeight.bold),
+                style: const TextStyle(
+                    fontSize: qtextsize, fontWeight: FontWeight.bold),
                 textAlign: TextAlign.center),
-          )
-      ),
+          )),
       //appBar: AppBar(title: Text("Pregunta #$questNum"),
       //),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                questText,
-                style: const TextStyle(fontSize: qtextsize),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(20)),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: sampleText
+      body: Form(
+        key: _formKey,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  questText,
+                  style: const TextStyle(fontSize: qtextsize),
+                  textAlign: TextAlign.center,
                 ),
-                onFieldSubmitted: (String value) async {
-                  try {
-                    if (inType == 'Int') {
-                      testlist[int.parse(questNum) - 1] = int.parse(value).toString();
-                    } else if (inType == 'Float') {
-                      testlist[int.parse(questNum) - 1] = double.parse(value.replaceAll(',','.')).toString();
-                    } else {
-                      testlist[int.parse(questNum) - 1] = value;
-                    }
-                  } catch (value) {}
-                },
               ),
-            )
-          ]),
+              const Padding(padding: EdgeInsets.all(20)),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: TextFormField(
+                  validator: (value) {
+                    print(value);
+                    //Se pide int y no es int
+                    if (inType == 'Int' && !isNumericInt(value ?? '')) {
+                      return 'eres retrasado, es int';
+                    } else if (inType == 'Float' &&
+                        !isNumericFloat(value ?? '')) {
+                      return 'eres retrasado, es float';
+                    }
+                    print(value);
+                     if (value == null || value.isEmpty) {
+                      return 'eres retrasado, es texto';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(hintText: sampleText),
+                  onFieldSubmitted: (String value) async {
+                    try {
+                      if (inType == 'Int') {
+                        testlist[int.parse(questNum) - 1] =
+                            int.parse(value).toString();
+                      } else if (inType == 'Float') {
+                        testlist[int.parse(questNum) - 1] =
+                            double.parse(value.replaceAll(',', '.')).toString();
+                      } else {
+                        testlist[int.parse(questNum) - 1] = value;
+                      }
+                    } catch (value) {}
+                  },
+                ),
+              )
+            ]),
+      ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.only(bottom: 20),
         child: BottomAppBar(
@@ -99,7 +132,9 @@ class InputPage extends StatelessWidget {
                   backgroundColor: const Color(0xffa9b4c2),
                   onPressed: () {
                     print(testlist);
-                    context.go(next); // Add your onPressed code here!
+                    if (_formKey.currentState!.validate()) {
+                      context.go(next);
+                    }
                   },
                   shape: const CircleBorder(),
                   child: const Icon(
